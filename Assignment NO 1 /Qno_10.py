a5581d09b943f9b47244faf46dc74c343d5d657f
ByteCodeVikash <vikash7618@gmail.com>
2026-04-07 08:14:55 +0530

    AssignmentNO1

---
+++ Assignment NO 1 /Qno_10.py
@@ -0,0 +1,12 @@
+"""
+10. Write a python script to print values of three variables, each in a new line. Variable
+contains integer values
+"""
+
+num1=25
+num2=35
+num3=45
+
+print(num1)
+print(num2)
+print(num3)
---
+++ Assignment NO 1 /Qno_9.py
@@ -0,0 +1,12 @@
+"""9. Write a python script to create variables to store your name, age, qualification, years
+of experience and print it."""
+
+name="Vikash"
+age=30
+qualification="B tech"
+year_of_experience="3 year"
+
+print(name)
+print(age)
+print(qualification)
+print(year_of_experience)
