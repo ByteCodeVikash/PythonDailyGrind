e19ffc1de58d03e2446ab715acab845742ce4093
ByteCodeVikash <vikash7618@gmail.com>
2026-04-11 19:01:06 +0530

    Assignment No 17

---
+++ Assignment No 17/Qno_2.py
@@ -0,0 +1,7 @@
+"""
+2. Write a python program to access the items of a dictionary by referring to its key
+name.
+"""
+d={'name':'Vikash','age':26,'geneder':'male'}
+
+print(d['name'])
---
+++ Assignment No 17/Qno_5.py
@@ -0,0 +1,6 @@
+#5. Write a python program to print all key names in the dictionary, one by one.
+
+d={'Name':'Vikash','age':26,'gender':'male'}
+
+for i in d:
+   print(i)
