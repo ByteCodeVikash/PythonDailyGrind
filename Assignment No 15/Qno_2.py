8ec9fb55880b8e556f7e22b2cf9921b9f8921f85
ByteCodeVikash <vikash7618@gmail.com>
2026-04-11 18:04:23 +0530

    Assignment No 15

---
+++ Assignment No 15/Qno_2.py
@@ -0,0 +1,5 @@
+#2. Write a python program to store only one item using tuple.
+
+item=("java",)
+
+print(item)
---
+++ Assignment No 15/Qno_3.py
@@ -0,0 +1,5 @@
+#3. Write a python program to reverse the tuple.
+
+item=("Java","Python","SQL","C")
+
+print(item[::-1])
